19ab66389b11672f887024336580599c43e6af8f
Levy Carneiro Jr <levy@levycarneiro.com>
2009-01-24 22:01:46 -0200

    Added method 'verify-key'.

--- lib/akismetor.rb
+++ lib/akismetor.rb
@@ -1,44 +1,51 @@
 class Akismetor
   attr_accessor :attributes
-  
+
+  require 'net/http'
+
+  # Does a key-check on Akismet so you know you can actually use a specific key.
+  # Returns "valid" or "invalid" depending on response.
+  def self.valid_key?(attributes)
+    self.new(attributes).execute('verify-key')
+  end
+
   # Does a comment-check on Akismet with the submitted hash.
   # Returns true or false depending on response.
   def self.spam?(attributes)
     self.new(attributes).execute('comment-check') != "false"
   end
-  
+
   # Does a submit-spam on Akismet with the submitted hash.
   # Use this when Akismet incorrectly approves a spam comment.
   def self.submit_spam(attributes)
     self.new(attributes).execute('submit-spam')
   end
-  
+
   # Does a submit-ham on Akismet with the submitted hash.
-  # Use this for a false positive, when Akismet incorrectly rejects a normal comment
+  # Use this for a false positive, when Akismet incorrectly rejects a normal comment.
   def self.submit_ham(attributes)
     self.new(attributes).execute('submit-ham')
   end
-  
-  
+
   def initialize(attributes)
     @attributes = attributes
   end
-  
+
   def execute(command)
-    http = Net::HTTP.new("#{attributes[:key]}.rest.akismet.com", 80)
-    response, content = http.post("/1.1/#{command}", attributes_for_post, http_headers)
-    content
+    host = "#{@attributes[:key]}." if attributes[:key] && command != 'verify-key'
+    http = Net::HTTP.new("#{host}rest.akismet.com", 80)
+    http.post("/1.1/#{command}", attributes_for_post, http_headers).body
   end
-  
-private
-  
+
+  private
+
   def http_headers
     {
-      'User-Agent' => 'Akismetor Rails Plugin/1.0',
+      'User-Agent' => 'Akismetor Rails Plugin/1.1',
       'Content-Type' => 'application/x-www-form-urlencoded'
     }
   end
-  
+
   def attributes_for_post
     result = attributes.map { |k, v| "#{k}=#{v}" }.join('&')
     URI.escape(result)
